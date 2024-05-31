a9dcd051ed9b46e01b6538a06972692d0a07c2de
mohsinbbx <mohsinkharadi51@gmail.com>
2024-05-31 16:05:38 +0530

    middleware intro

--- 005 rende-html-and-file/index.js
+++ 005 rende-html-and-file/index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
 
 const app = express();
 
+const htmlPath = path.join(__dirname, 'public');
+
+app.use(express.static(htmlPath));
+
 app.get('/',(req,res) => {
-    res.send('api called');
+    res.sendFile(`${htmlPath}/home.html`);
+})
+
+app.get('/about',(req,res) => {
+    res.sendFile(`${htmlPath}/about.html`);
+})
+
+app.get('/contact',(req,res) => {
+    res.sendFile(`${htmlPath}/contact.html`);
+})
+
+app.get('/pdf',(req,res) => {
+    res.sendFile(`${htmlPath}/file-sample_150kB.pdf`);
+})
+
+app.get('/*', (req,res) => {
+    res.sendFile(`${htmlPath}/404.html`);
 })
 
 app.listen(process.env.PORT, () => {
---
+++ 006 middleware/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const checkToken = (req,res,next)=>{
+    console.log('object');
+
+    next();
+};
+
+app.get('/user', checkToken, (req,res)=>{
+    res.send('data fatched successfully !!');
+})
+
+app.listen(5500,()=>{
+    console.log('server is running on port 5500');
+})
